3edefe7766408bbd90224d61cafcf63918ed81d4
mauriValdi <mauricioo.valdi@hotmail.com>
2017-02-22 03:32:05 -0400

    condiciones mas claras en metodos aparte

--- TennisGame2.java
+++ TennisGame2.java
@@ -10,44 +10,68 @@ public class TennisGame2 implements TennisGame
     	String P1res = "";
     	String P2res = "";
         String literalScore = "";
-        if (player1Points == player2Points && player1Points < 4)
-            literalScore = getDrawResult(player1Points);
-        if (player1Points==player2Points && player1Points>=3)
+        if (scoreIsTied(player1Points, player2Points))
+            literalScore = getTieResult(player1Points);
+        if (scoreIsDeuce(player1Points, player2Points))
             literalScore = "Deuce";
-        if (player1Points > 0 && player2Points==0)
+        if (playerIsWinningToCero(player1Points, player2Points))
         {
             P1res = getPlayerLiteralPoints(player1Points);
             P2res = "Love";
             literalScore = P1res + "-" + P2res;
         }
-        if (player2Points > 0 && player1Points==0)
+        if (playerIsWinningToCero(player2Points, player1Points))
         {
             P2res = getPlayerLiteralPoints(player2Points);
             P1res = "Love";
             literalScore = P1res + "-" + P2res;
         }  
-        if (player1Points>player2Points && player1Points < 4 && player2Points > 0)
+        if (playerIsWinning(player1Points, player2Points))
         {
             P1res = getPlayerLiteralPoints(player1Points);
             P2res = getPlayerLiteralPoints(player2Points);
             literalScore = P1res + "-" + P2res;
         }
-        if (player2Points>player1Points && player2Points < 4 && player1Points > 0)
+        if (playerIsWinning(player2Points, player1Points))
         {
             P2res = getPlayerLiteralPoints(player2Points);
             P1res = getPlayerLiteralPoints(player1Points);
             literalScore = P1res + "-" + P2res;
         }
-        if (player1Points > player2Points && player2Points >= 3)
+        if (playerHasAdvantage(player1Points, player2Points))
             literalScore = "Advantage player1";
-        if (player2Points > player1Points && player1Points >= 3)
+        if (playerHasAdvantage(player2Points, player1Points))
             literalScore = "Advantage player2";
-        if (player1Points>=4 && player2Points>=0 && (player1Points-player2Points)>=2)
+        if (playerWon(player1Points, player2Points))
             literalScore = "Win for player1";
-        if (player2Points>=4 && player1Points>=0 && (player2Points-player1Points)>=2)
+        if (playerWon(player2Points, player1Points))
             literalScore = "Win for player2";
         return literalScore;
     }
+
+	private boolean playerWon(int firstPlayerPoints, int secondPlayerPoints) {
+		return firstPlayerPoints>=4 && secondPlayerPoints>=0 && (firstPlayerPoints-secondPlayerPoints)>=2;
+	}
+
+	private boolean playerHasAdvantage(int firstPlayerPoints, int secondPlayerPoints) {
+		return firstPlayerPoints > secondPlayerPoints && secondPlayerPoints >= 3;
+	}
+
+	private boolean playerIsWinning(int firstPlayerPoints, int secondPlayerPoints) {
+		return firstPlayerPoints>secondPlayerPoints && firstPlayerPoints < 4 && secondPlayerPoints > 0;
+	}
+
+	private boolean playerIsWinningToCero(int firstPlayerPoints, int secondPlayerPoints) {
+		return firstPlayerPoints > 0 && secondPlayerPoints==0;
+	}
+
+	private boolean scoreIsDeuce(int firstPlayerPoints, int secondPlayerPoints) {
+		return firstPlayerPoints==secondPlayerPoints && firstPlayerPoints>=3;
+	}
+
+	private boolean scoreIsTied(int fisrtPlayerPoints, int secondPlayerPoints) {
+		return fisrtPlayerPoints == secondPlayerPoints && fisrtPlayerPoints < 4;
+	}
 
 	private String getPlayerLiteralPoints(int playerPoints) {
 		if (playerPoints==1)
@@ -60,16 +84,16 @@ public class TennisGame2 implements TennisGame
 	}
 
 
-	private String getDrawResult(int points) {
-		String aux = "";
+	private String getTieResult(int points) {
+		String literal = "";
 		if (points==0)
-			aux = "Love";
+			literal = "Love";
 		if (points==1)
-			aux = "Fifteen";
+			literal = "Fifteen";
 		if (points==2)
-			aux=  "Thirty";
-		aux += "-All";
-		return aux;
+			literal=  "Thirty";
+		literal += "-All";
+		return literal;
 	}
     
     public void setPlayer1Score(int potins){
